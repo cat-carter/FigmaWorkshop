e99eed8e056bf2f682d34e5b45c302d281a92b20
Catherine Carter <cvcarter@miami.edu>
2026-03-03 22:33:20 +0000

    java

---
+++ JS/imagegallery.js
@@ -0,0 +1,9 @@
+"use strict"
+
+let galleryClass.= document.querySelectorAll(".imageGallery");
+console.log(galleryClass);
+console.log(galleryClass.length);
+
+galleryClass[0].style.display = "none";
+galleryClass[1].style.display = "none";
+galleryClass[2].style.display = "none";
